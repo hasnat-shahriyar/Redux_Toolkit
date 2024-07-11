38907d542ec789e3545c19953fad5fc0e5d2fd90
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-07-11 21:14:19 +0600

    addTodo functionality and css

--- Redux_Learning/redux_L01/src/components/AddTodo/AddTodo.jsx
+++ Redux_Learning/redux_L01/src/components/AddTodo/AddTodo.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
-
+import { addTodo } from '../../features/todo/todoSlice';
+import { useDispatch } from require('react-redux');
 const AddTodo = () => {
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState('');
+  const dispatch = useDispatch();
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input));
+    setInput('');
+  }
   return (
     <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
       <input
